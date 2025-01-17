b1515103dfab60601bf788bb3b7a0fde40cfa849
Eren <erenalparslan52@gmail.com>
2025-01-17 18:47:00 +0300

    Result Progress added

--- app/screens/Details/index.tsx
+++ app/screens/Details/index.tsx
@@ -4,34 +4,36 @@ import { GET_QUESTION_DETAIL } from "@/app/graphql/queries";
 import { useQuery } from "@apollo/client";
 import Loading from "@/app/components/Loading";
 import { Box, Divider, Heading } from "native-base";
-import Form from "./Form"
+import Form from "./Form";
 import Result from "../Results/Result";
 
 export default function DetailsScreen({ route }) {
   const { id } = route.params;
-  const [isVoted,setIsVoted] =useState(false);
+  const [isVoted, setIsVoted] = useState(false);
   const { loading, error, data } = useQuery(GET_QUESTION_DETAIL, {
     variables: { id },
     fetchPolicy: "network-only",
   });
 
+  if (loading) {
+    return <Loading />;
+  }
 
-    if (loading) {
-        return <Loading />
-    }
-
-    if (error) {
-        return <Text>{JSON.stringify(error)}</Text>
-    }
+  if (error) {
+    return <Text>{JSON.stringify(error)}</Text>;
+  }
 
   console.log("DetailsScreen, item", data);
   const { text, options } = data.questions_by_pk;
   return (
     <Box p="3">
-    <Heading >{text}</Heading>
-    <Divider my={2} />
-    {!isVoted?(  <Form  options={options} setIsVoted={setIsVoted}  />):(<Result id={id}/>)}
-
-</Box>
+      <Heading>{text}</Heading>
+      <Divider my={2} />
+      {!isVoted ? (
+        <Form options={options} setIsVoted={setIsVoted} />
+      ) : (
+        <Result id={id} />
+      )}
+    </Box>
   );
 }
--- app/screens/Results/ResultItem.tsx
+++ app/screens/Results/ResultItem.tsx
@@ -1,14 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Box, Heading } from 'native-base'
+import { View, Text } from "react-native";
+import React from "react";
+import { Box, Heading, Progress } from "native-base";
 
-const ResultItem = ({item,total}) => {
+const ResultItem = ({ item, total }) => {
   return (
-    <Box>
-        <Heading size={"sm"} mb={"3"}>{item.text} (%{((item.answers_aggregate.aggregate.count *100) /total).toFixed(1)}) {"-"} {item.answers_aggregate.aggregate.count} total </Heading>
+    <Box my={"3"}>
+      <Heading size={"sm"} mb={"3"}>
+        {item.text} (%
+        {((item.answers_aggregate.aggregate.count * 100) / total).toFixed(1)}){" "}
+        {"-"} {item.answers_aggregate.aggregate.count} total{" "}
+      </Heading>
+      
+      <Progress
+        size={"lg"}
+        value={item.answers_aggregate.aggregate.count}
+        max={total}
 
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default ResultItem
+export default ResultItem;
